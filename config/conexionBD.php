5b23c35a994c2371f0dfe7448898d10785707258
ivanx21 <isanchezg1@est.ups.edu.ec>
2021-12-16 17:23:09 -0500

    -Inicio sesion de cliente terminado.

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -13,4 +13,27 @@
         // echo"<p> Conexión exitosa!!</p>";
     }
 
+  session_start();
+  var_dump($_POST);
+  include '../../config/conexionBD.php';
+  $correo = isset($_POST["correo"]) ? trim($_POST["correo"]): null;
+  $contrasena = isset($_POST["clave"]) ? trim($_POST["clave"]) : null;
+
+      $sql = "SELECT usu_codigo, usu_correo, usu clave, usu_rol FROM usuario WHERE usu_correo='$correo' AND usu_contrasena='$contrasena'";
+
+      $result = mysqli_query($conn, $sql);
+
+      if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          //echo "id: " . $row["id"]. " " . $row["correo"]. " " . $row["contrasena"]. " " . $row["rol"] . "<br>";
+          $sqlconsulta = "SELECT nombre FROM cliente WHERE usu_codigo=' $row[id] '";
+          $_SESSION['Inicio'] = $row["id"];
+        }
+      } else {
+        echo "0 results";
+      }
+      mysqli_close($conn);
+      //echo $_SESSION['Inicio'];
+
 ?>
